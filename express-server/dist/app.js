184cd739969d5df35c89c4d3103325c37886d7c7
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-08-26 23:39:54 -0700

    dotenv config

--- express-server/dist/app.js
+++ express-server/dist/app.js
@@ -3,11 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// Third part imports
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Middleware
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+// Routes
 app.get("/", (req, res) => {
     res.send("Hello from AuthKit");
 });
-app.listen(5000, () => {
-    console.log("App running on port 5000");
+app.listen(process.env.PORT, () => {
+    console.log(`App running on port ${process.env.PORT}`);
 });
--- express-server/src/app.ts
+++ express-server/src/app.ts
@@ -1,11 +1,17 @@
+// Third part imports
 import express from "express";
+import dotenv from "dotenv";
+
+// Middleware
+dotenv.config();
 
 const app = express();
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello from AuthKit");
 });
 
-app.listen(5000, () => {
-  console.log("App running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port ${process.env.PORT}`);
 });
