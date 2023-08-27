dd19bcf41869286b119da48df83cb5777bca393d
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-08-26 23:22:41 -0700

    ui and tailwind set up

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <>AuthKit</>;
+  return (
+    <>
+      <div className="flex flex-col gap-4 p-8 items-center">
+        AuthKit App
+        <Button>Get Started</Button>
+      </div>
+    </>
+  );
 }
 
 export default App;
